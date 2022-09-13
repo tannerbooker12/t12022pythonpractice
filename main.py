4047b79c2c1499d28ed14f91e506a628daab0f50
tannerbooker12 <829075@mystma.org>
2022-09-13 09:41:34 -0500

    3 functions

---
+++ main.py
@@ -0,0 +1,21 @@
+def addTwoNumbers():
+    x=3
+    y=2
+    print(y+x)
+
+addTwoNumbers()
+
+def iLikeItWhenTheyCallMe():
+    print("BigPapa")
+
+def name():
+    print("tbooks")
+
+def topFiveWhatevers():
+    print("callofduty")
+    print("ghostoftsushima")
+    print("GodOfWar")
+    print("LastOfUs")
+    print("halo3")
+
+    
